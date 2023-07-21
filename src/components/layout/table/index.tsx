e4831e388d4d73c292a3a82a0b6acce576e44c33
devYuMinKim <gimyumin40@gmail.com>
2023-07-21 14:05:03 +0900

    ✨ feat : Implement Committee page

---
+++ src/components/layout/table/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface CommitteeMember {
+  id: number;
+  name: string;
+  affiliation: string;
+  note: string;
+  pivot: {
+    cId: number;
+    id2: number;
+    note: string;
+  };
+}
+
+const CommitteeTableRow: React.FC<{ member: CommitteeMember }> = ({
+  member,
+}) => (
+  <tr>
+    <td className="p-2 whitespace-nowrap">
+      <div className="font-medium text-gray-800">{member.name}</div>
+    </td>
+    <td className="p-2 whitespace-nowrap">
+      <div className="text-left">{member.affiliation}</div>
+    </td>
+    <td className="p-2 whitespace-nowrap">
+      <div className="text-left font-medium text-green-500">
+        {member.pivot.note}
+      </div>
+    </td>
+  </tr>
+);
+
+export default CommitteeTableRow;
