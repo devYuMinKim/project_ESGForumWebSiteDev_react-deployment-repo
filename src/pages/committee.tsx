45ea3bee87bc12a4f228cb092268e9a54a7512cc
devYuMinKim <gimyumin40@gmail.com>
2023-07-27 00:35:04 +0900

    💄 style : Add margin-top

--- src/pages/committee.tsx
+++ src/pages/committee.tsx
@@ -35,7 +35,7 @@ const Committee: React.FC = () => {
   }, []);
 
   return (
-    <section className="antialiased bg-gray-100 text-gray-600 h-screen px-4">
+    <section className="antialiased mt-10	text-gray-600 h-screen px-4">
       <div className="flex flex-col justify-center">
         {committees.map((committee) => (
           <div
