4448fa5c9d6ebc9f80fabfc9dec3fbf80b5d1cef
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-08-24 15:31:38 +0900

    feat(seminar): create services (getAllSeminars, getSeminarById, createSeminar, updateSeminar, deleteSeminar)

---
+++ src/services/seminar.service.ts
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import { Seminar, SendSeminar } from '../types/seminars.interface';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+/**
+ * /seminars
+ * Get all seminars
+ * @returns Seminar[]
+ */
+export async function getAllSeminars(): Promise<Seminar[]> {
+  const res = await axios.get<Seminar[]>(`${API_URL}/seminars`);
+  return res.data;
+}
+
+/**
+ * /seminars/:id
+ * Get seminar by id
+ * @param id
+ * @returns Seminar
+ */
+export async function getSeminarById(id: string | number): Promise<Seminar> {
+  const res = await axios.get<Seminar>(`${API_URL}/seminars/${id}`);
+  return res.data;
+}
+
+/**
+ * /seminars
+ * Create seminar
+ * @param seminar
+ * @returns Seminar
+ */
+export async function createSeminar(seminar: SendSeminar): Promise<Seminar> {
+  const res = await axios.post<Seminar>(`${API_URL}/seminars`, seminar);
+  return res.data;
+}
+
+/**
+ * /seminars/:id
+ * Update seminar
+ * @param id
+ * @param seminar
+ * @returns Seminar
+ */
+export async function updateSeminar(id: string | number, seminar: SendSeminar): Promise<Seminar> {
+  const res = await axios.put<Seminar>(`${API_URL}/seminars/${id}`, seminar);
+  return res.data;
+}
+
+/**
+ * /seminars/:id
+ * Delete seminar
+ * @param id
+ * @returns Seminar
+ */
+export async function deleteSeminar(id: string | number) {
+  const res = await axios.delete(`${API_URL}/seminars/${id}`);
+  return res.data;
+}
