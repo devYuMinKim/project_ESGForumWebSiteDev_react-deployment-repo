2de61ded310be18fed381fe0ba0809bdf1636c28
Z00One <9000248@g.yju.ac.kr>
2023-08-28 01:06:39 +0900

    feat(statisticsCard): Add support for member data

--- src/components/layout/dashboard/statisticsCard.tsx
+++ src/components/layout/dashboard/statisticsCard.tsx
@@ -1,21 +1,22 @@
-import { createElement } from 'react';
+import React, { createElement } from 'react';
 import StatisticsCard from '../../widget/cards/statisticsCard';
-import { StatisticsCardData } from '../../../data';
+import { Member, StatisticsCardData } from '../../../data';
+import { CommitteeData } from '../../../data';
 
-interface AssetData<T> {
-  committees?: T[]
-  committee?: T[]
+interface AssetData {
+  committees?: CommitteeData[]
+  committee?: CommitteeData[]
+  members?: Member[]
 }
 
-export interface StatisticsCardsSectionProps<T> {
+export interface StatisticsCardsSectionProps {
   statisticsCardsData: StatisticsCardData[]
-  assetData: AssetData<T>
+  assetData: AssetData
 }
-
-const StatisticsCardsSection = <T extends unknown>({
+ 
+const StatisticsCardsSection: React.FC<StatisticsCardsSectionProps> = ({
   statisticsCardsData,
-  assetData
-}: StatisticsCardsSectionProps<T>) => {
+  assetData }) => {
   return (
     <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-3 bg-clip-border">
       {statisticsCardsData.map(({ icon, name, color, title, value, ...rest }) => (
@@ -24,7 +25,7 @@ const StatisticsCardsSection = <T extends unknown>({
           {...rest}
           title={title}
           color={color}
-          value={value ? value : assetData[name as keyof AssetData<T>]?.length || 0}
+          value={value ? value : assetData[name as keyof AssetData]?.length || 0}
           icon={createElement(icon, {
             className: "w-6 h-6 text-white",
           })}
