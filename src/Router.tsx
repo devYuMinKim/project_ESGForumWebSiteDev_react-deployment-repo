5742e9aba0deb216b3ae484da1acb73fddc7eb70
devYuMinKim <gimyumin40@gmail.com>
2023-08-31 04:11:02 +0900

    fix(Router): fix overlapping urls

--- src/Router.tsx
+++ src/Router.tsx
@@ -21,25 +21,6 @@ import CommitteeInfo from "./pages/admin/committee";
 import SeminarDetailPage from "./pages/seminar_detail";
 import ReferenceDetailPage from "./pages/reference_detail";
 import NotificationDetailPage from "./pages/notification_detail";
-import DashBoard from "./pages/admin/dashboard";
-import CommitteeInfo from "./pages/admin/committee";
-import SeminarDetailPage from "./pages/seminar_detail";
-import ReferencePage from "./pages/reference";
-import NotificationPage from "./pages/notification";
-import AboutUsPage from './pages/aboutus';
-import ObjectivePage from './pages/aboutus/objective';
-import BoEPage from './pages/aboutus/boe';
-import MeetingsPage from './pages/aboutus/meetings';
-import HistoryPage from './pages/aboutus/history';
-import RulesPage from './pages/aboutus/rules';
-import ContactPage from './pages/aboutus/contact';
-import SeminarPage from './pages/seminars';
-import DashBoard from './pages/admin/dashboard';
-import CommitteeInfo from './pages/admin/committee';
-import QuillEditor from './components/editor/quill-editor';
-import Editor from './pages/admin/post';
-import Members from './pages/admin/members';
-
 
 const Router: React.FC = () => (
   <Routes>
