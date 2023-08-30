364ab62941d93100f8f82cc61a11a174f16b49fe
devYuMinKim <gimyumin40@gmail.com>
2023-08-30 10:43:37 +0900

    refactor(seminar): remove unnecessary methods

--- src/pages/seminars.tsx
+++ src/pages/seminars.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {
   getSeminars,
-  getSeminarById,
   getOngoingSeminars,
   getPastSeminars,
   searchSeminars,
