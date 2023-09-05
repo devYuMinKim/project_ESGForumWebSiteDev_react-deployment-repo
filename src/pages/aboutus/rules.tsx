e30214463cd866f53ca07c6defe7dbb2eb817089
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-09-05 15:29:55 +0900

    test: add console.log for temporary testing

--- src/pages/aboutus/rules.tsx
+++ src/pages/aboutus/rules.tsx
@@ -1,13 +1,17 @@
 import { Body, ContentContainer } from '../../components/aboutus/Layout';
 import PageTitle from '../../components/aboutus/PageTitle';
+import QuillEditor from '../../components/editor/quill-editor';
 import SubNav from '../../components/layout/subnav';
 
 const RulesPage = () => {
+  console.log(process.env.REACT_APP_API_URL);
   return (
     <Body>
       <PageTitle background="#ADCF9F">운영규율</PageTitle>
       <SubNav />
-      <ContentContainer></ContentContainer>
+      <ContentContainer>
+        <QuillEditor />
+      </ContentContainer>
     </Body>
   );
 };
